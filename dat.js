a50cf6328d5ffb0e5273153da24ffe3d68f34621
aditya-nambiar2007 <99115432+aditya-nambiar2007@users.noreply.github.com>
2025-06-15 12:14:45 +0530

    Add files via upload

--- dat.js
+++ dat.js
@@ -2,7 +2,8 @@ const topics={
     birds   : ["Pigeon", "Crow", "Sparrow", "Peacock", "Parrot", "Duck", "Goose", "Owl", "Eagle", "Hawk", "Robin", "Woodpecker", "Kingfisher", "Dove", "Swallow", "Heron", "Pelican", "Macaw", "Penguin", "Ostrich", "Seagull", "Stork", "Flamingo", "Cuckoo", "Hummingbird", "Turkey", "Sunbird", "Rooster", "Hen", "Chicken",  "Falcon", "Cockatoo", "Toucans", "Canary", "Nightingale", "Vulture", "Swan", "Dodo", "Kiwi", "Albatross"],
     animals : ["Dog", "Cat", "Cow", "Horse", "Elephant", "Lion", "Tiger", "Bear", "Wolf", "Fox", "Rabbit", "Deer", "Giraffe", "Zebra", "Kangaroo", "Panda", "Hippopotamus", "Rhinoceros", "Crocodile", "Alligator", "Tortoise", "Crab", "Seal", "Armadillo", "Beaver", "Dolphin", "Whale", "Shark", "Octopus", "Turtle", "Frog", "Snake", "Lizard", "Mouse", "Rat", "Squirrel", "Bison", "Buffalo", "Chimpanzee", "Gorilla"],
     flowers : ["Rose", "Tulip", "Sunflower", "Daisy", "Lily", "Marigold", "Jasmine", "Orchid", "Daffodil", "Lavender","Hibiscus","Violet","Dahlia"],
-    numbers : Array.from({length: 100}, (_, i) => i + 1)
+    numbers : Array.from({length: 100}, (_, i) => i + 1),
+    sports:["Soccer", "Basketball", "Cricket", "Tennis", "Baseball", "Golf", "Swimming", "Badminton", "Volleyball", "Table Tennis"]
 }
 
 module.exports=topics
