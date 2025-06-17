2d665b47d2d6fb4725f009c14831c1f65751ee77
aditya-nambiar2007 <99115432+aditya-nambiar2007@users.noreply.github.com>
2025-06-17 13:27:43 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+// Import required modules
 const fs = require("fs")
 const url = require('url')
+
+// Create an HTTP server
 const http = require('http').createServer((req, res) => {
+    // Handle request to join a room
     if (req.url == "/join") {
         fs.readFile("./open.html", function (error, content) {
             res.writeHead(200, { 'Content-Type': "text/html" });
             res.end(content, 'utf-8');
         })
-
     }
+    // Handle request to check if a room is available
     else if (req.path == "/room") {
         let response;
         try {
@@ -15,12 +19,11 @@ const http = require('http').createServer((req, res) => {
         } catch (error) {
             response = true
         }
-
         const room = url.parse(req.url, true).query["room"]
         res.writeHead(200, { 'Content-Type': "text/html" });
         res.end(response, 'utf-8');
-
     }
+    // Serve the main index.html for all other requests
     else {
         fs.readFile("./index.html", function (error, content) {
             res.writeHead(200, { 'Content-Type': "text/html" });
@@ -28,86 +31,107 @@ const http = require('http').createServer((req, res) => {
         })
     }
 })
+
+// Import topics from external file
 const topics = require("./dat.js")
 
+// Function to shuffle an array
 const array_sort = (array) => { return array.slice().sort(() => 0.5 - Math.random()) }
-//topics
 
-
+// Set up Socket.io for real-time communication
 const io = require('socket.io')(http, { cors: { origin: "*" } });
+
+// In-memory storage for rooms, sockets, and odd player assignment
 let rooms = {}
 let sockets = {}
 let odds = {}
+let votes = {}
 io.on("connection", socket => {
-    //connection:
+    // Store connected socket
     sockets[socket.id] = socket
     let room, name;
 
-    //join:
+    // Handle join event: add player to a room
     socket.on("join", data => {
         room = data.room
         name = data.name
         socket.gameName = data.name
-        if (!rooms[room]) { socket.emit('host') }
-        rooms[room] = rooms[room] || [];
+        if (!rooms[room]) { socket.emit('host') } // Notify if user is host (first in room)
+        rooms[room] = rooms[room] || []
         rooms[room].push(socket)
+        // Build a list of members in the room
         let members = []
-        rooms[room].forEach(e => { members.push({ id: e.id, name: e.gameName }) });
-        rooms[room].forEach(e => { e.emit("members", members) });
+        rooms[room].forEach(e => { members.push({ id: e.id, name: e.gameName }) })
+        // Notify all members of the updated member list
+        rooms[room].forEach(e => { e.emit("members", members) })
 
-        console.log(socket.id, " Joined ", room);
-        console.log(rooms[room])
     })
 
-    //start
+    // Broadcast topic selection to all members in the room
     socket.on("topic", str => { rooms[room].forEach(e => { e.emit("topic", str) }) })
 
+    // Handle game start event
     socket.on("start", (topic) => {
-        if (rooms[room].length > 4) {
-            rooms[room].push("going")
-        odds[room] = rooms[room][Math.floor(Math.random() * rooms[room].length)]
-        let words = array_sort(topics[topic])
-        rooms[room].forEach(e => {
-            if (e == odds[room]) {
-                e.emit("word", words[0])
-                e.emit("time",60*rooms[room].length)
-            } else {
-                e.emit("word", words[1])
-                e.emit("time",60*rooms[room].length)
-            }
-        });
-            setTimeout(rooms[room].forEach, 60000*rooms[room].length,e => { e.emit("end") })
-        }
-        else {
-        socket.emit("error", 1)
+        if (rooms[room].length > 4) { // Minimum players required
+            rooms[room].push("going") 
+            // Mark the room as in-game
+            // Randomly select one player as the "impostor" (odd one out)
+            odds[room] = rooms[room][Math.floor(Math.random() * rooms[room].length)]
+            // Shuffle topic words
+            let words = array_sort(topics[topic])
+            // Assign words and timer to each player
+            rooms[room].forEach(e => {
+                try {
+                    if (e == odds[room]) {
+                        e.emit("word", words[0])
+                    } else {
+                        e.emit("word", words[1])
+                    }
+                    e.emit("time", 60 * rooms[room].length)
+
+                } catch (error) {
+                }
+
+            })
+            // End the game after a set time
+            setTimeout(rooms[room].forEach, 60000 * rooms[room].length, e => { e.emit("end"); votes[room][e.id] = 0 })
+            setTimeout(rooms[room].forEach, 60000 * rooms[room].length + 60000, e => { e.emit("votes", votes[room]) })
+        } else {
+            // Not enough players to start
+            socket.emit("error", 1)
         }
     })
 
-
-    //message
+    // Broadcast chat messages to all members in the room
     socket.on("msg", data => {
-        rooms[room].forEach(e => { e.emit("msg", data) });
+        rooms[room].forEach(e => { e.emit("msg", data) })
     })
 
-    //dms
+    // Direct messages to selected users
     socket.on("dm", d => {
         d.users.forEach(element => {
             sockets[element].emit("msg", d.msg)
-        });
+        })
     })
 
-    //disconnection
+    //Handle votes
+    socket.on("vote", id => { votes[room][id]++ })
 
+    // Handle user disconnect
     socket.on("disconnect", () => {
+        // Remove the socket from the room
         if (rooms[room].indexOf(socket) > -1) {
-            rooms[room].splice(rooms[room].indexOf(socket), 1);
+            rooms[room].splice(rooms[room].indexOf(socket), 1)
         }
+        // Delete the room if empty
         if (rooms[room].length == 0) { delete rooms[room] }
-        rooms[room].forEach(e => { e.emit("members", members) });
+        // Notify remaining members of the updated member list
+        rooms[room].forEach(e => { e.emit("members", members) })
         delete sockets[socket.id]
-        console.log(socket.id, " Left ", room);
-        console.log(rooms[room]);
+        console.log(socket.id, " Left ", room)
+        console.log(rooms[room])
     })
 })
 
+// Start listening on port 8080
 http.listen(8080, "0.0.0.0")
