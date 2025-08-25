fbf5a6dfb2cdbef06ad58200f75c7e1db013f956
aditya-nambiar2007 <99115432+aditya-nambiar2007@users.noreply.github.com>
2025-08-25 18:58:59 +0530

    Add files via upload

--- JS/db.js
+++ JS/db.js
@@ -1,61 +1,58 @@
-// Import the MongoDB client from the 'mongodb' package
-const db = require("mongodb").MongoClient
+const { MongoClient } = require("mongodb");
+const url = 'mongodb://127.0.0.1:27017/';
+const dbName = "clients";
 
-// Module that provides functions to interact with the MongoDB database
-const mod = {
-    // Creates a new collection (room) in the 'clients' database
-    create_room: (room) => {
-        db.connect('mongodb://localhost:27017/', (err, db) => {
-            dbo = db.db("clients")
-            dbo.createCollection(room)
-        })
-    },
-
-    // Inserts a new document with sid, odd_one, and name into the specified room (collection)
-    insert: (sid, odd_one, name, room) => {
-        db.connect('mongodb://localhost:27017/', (err, db) => {
-            dbo = db.db("clients")
-            dbo.collection(room).insertOne({ sid: sid, odd_one: odd_one, name: name })
-        })
-    },
-
-    // Deletes a document by sid and room from the 'clients' database
-    delete: (sid, room) => {
-        db.connect('mongodb://localhost:27017/', (err, db) => {
-            dbo = db.db("clients")
-            dbo.deleteOne({ sid: sid, room: room })
-        })
-    },
-
-    // Drops (deletes) the entire room (collection) from the 'clients' database
-    delete_room: (room) => {
-        db.connect('mongodb://localhost:27017/', (err, db) => {
-            dbo = db.db("clients")
-            dbo.dropCollection(room)
-        })
-    },
-
-    // Updates the 'odd_one' field of a document identified by sid in the specified room
-    edit: (sid, v, room) => {
-        db.connect('mongodb://localhost:27017/', (err, db) => {
-            dbo = db.db("clients")
-            dbo.collection(room).updateOne({ sid: sid }, { $set: { odd_one: v } })
-        })
-    },
-
-    // Reads all documents from the specified room (collection)
-    read: (room) => {
-        let result=0;
-        db.connect('mongodb://localhost:27017/', (err, db) => {
-            dbo = db.db("clients")
-            dbo.collection(room).find({}).toArray((err, res) => {
-                result = res;
-            });
-
-        })
-        return result;
-    }
+async function getDb() {
+    const client = new MongoClient(url);
+    await client.connect();
+    return client;
 }
 
-// Export the module so it can be used in other parts of the application
-module.exports = mod
+const mod = {
+    create_room: async (room) => {
+        const client = await getDb();
+        const dbo = client.db(dbName);
+        await dbo.createCollection(room);
+        await client.close();
+    },
+
+    insert: async (sid, odd_one, name, room) => {
+        const client = await getDb();
+        const dbo = client.db(dbName);
+        await dbo.collection(room).insertOne({ sid, odd_one, name });
+        await client.close();
+    },
+
+    delete: async (sid, room) => {
+        const client = await getDb();
+        const dbo = client.db(dbName);
+        await dbo.collection(room).deleteOne({ sid });
+        await client.close();
+    },
+
+    delete_room: async (room) => {
+        const client = await getDb();
+        const dbo = client.db(dbName);
+        await dbo.dropCollection(room);
+        await client.close();
+    },
+
+    edit: async (sid, v, room) => {
+        const client = await getDb();
+        const dbo = client.db(dbName);
+        await dbo.collection(room).updateOne({ sid }, { $set: { odd_one: v } });
+        await client.close();
+    },
+
+    read: async (room) => {
+        const client = await getDb();
+        const dbo = client.db(dbName);
+        const result = await dbo.collection(room).find({}).toArray();
+        await client.close();
+        return new Promise((resolve, reject) => {
+            result ? resolve(result) : reject(false);
+        });
+    }
+};
+
+module.exports = mod;
