e106b0338a3a144ba30c2e362e1db6c3297d08a8
aditya-nambiar2007 <99115432+aditya-nambiar2007@users.noreply.github.com>
2025-08-29 00:24:48 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const url = require('url')
 
 const http = require('http').createServer((req, res) => {
 
+    //Server Handling
     if (req.url == "/join") {
         fs.readFile("./open.html", function (error, content) {
             res.writeHead(200, { 'Content-Type': "text/html" });
@@ -13,9 +14,9 @@ const http = require('http').createServer((req, res) => {
 
     else if (req.path == "/room") {
         let response;
-        db.read(room).then(cont=>response=cont[-1])
+        db.read(room).then(cont => response = cont[-1])
         try {
-            response =  response != "going"
+            response = response != "going"
         } catch (error) {
             response = true
         }
@@ -44,46 +45,52 @@ const io = require('socket.io')(http, { cors: { origin: "*" } });
 
 let sockets = {}
 let odds = {}
-let votes = {}
 let messages = {}
+
+//Socketio initiation
 io.on("connection", socket => {
 
     sockets[socket.id] = socket
-    let room, name;
+    let room, name, colour;
 
-
+    //Event initiation
     socket.on("join", async data => {
         room = data.room
         name = data.name
+        colour = data.colour
 
-        let res= await db.read(room).catch(()=>false)
-        console.log(res);
-        
-                if (!res) { socket.emit('host');db.create_room(room);db.insert(socket.id,0,name,room) }
-                else {
-                    sockets[socket.id] = socket
-                    db.insert(socket.id, 0, name, room)
+        let res = await db.read(room).catch(() => false)
 
-                    let members = []
-                    db.read(room).then(cont=>cont.forEach(e => { members.push({ id: e.sid, name: e.name }) }))
+        //If room is empty make the player a host else make him a normal player
+        if (res.length == 0) {
+            socket.emit('host');
+            db.create_room(room);
+            console.log("Host Maken");
+        }
 
-                    db.read(room).then(cont=>cont.forEach(e => { console.table(members)  ;sockets[e.sid].emit("members", members) }))
+        sockets[socket.id] = socket
+        db.insert(socket.id, 0, name, [], room, colour)
+        res = await db.read(room).catch(() => false)
+        let members = []
+        db.read(room).then(cont => cont.forEach(e => { members.push({ id: e.sid, name: e.name ,colour:e.colour}) }))
 
-                    
-                }
-            
+        db.read(room).then(cont => cont.forEach(e => { sockets[e.sid].emit("members", members) }))
+
+
+
     })
 
+    //Real time topic updates
+    socket.on("topic", str => { db.read(room).then(cont => cont.forEach(e => { sockets[e.sid].emit("topic", str) })) })
 
-    socket.on("topic", str => { db.read(room).then(cont=>cont.forEach(e => { sockets[e.sid].emit("topic", str) })) })
-
+    //Start of game
     socket.on("start", async (topic) => {
-        let content= await db.read(room).catch(()=>false)
-        if ( content.length > 3) {
+        let content = await db.read(room).catch(() => false)
+        if (content.length >= 3) {
             odds[room] = content[Math.floor(Math.random() * content.length)]
 
             let words = array_sort(topics[topic])
-
+            //make impostor
             content.forEach(e => {
                 try {
                     if (e == odds[room]) {
@@ -93,53 +100,71 @@ io.on("connection", socket => {
                     }
                     sockets[e.sid].emit("time", 60 * content.length)
                     messages[room] = 0;
-                    socket.emit("chance")
+                    //socket.emit("chance")
                 } catch (error) {
                 }
 
             })
+             
+            //Game End
+            setTimeout(() => {                
+                content.forEach(e => {
+                    sockets[e.sid].emit("end"); console.log("end");
+                })
+            }, 60000 * content.length)
 
-            setTimeout(content.forEach, 60000 * content.length, e => { sockets[e.sid].emit("end"); votes[room][e.id] = 0 })
-            setTimeout(content.forEach, 60000 * content.length + 60000, e => { sockets[e.sid].emit("votes", votes[room], odds[room]) })
+            //Vote End
+            setTimeout(() => {
+                content.forEach(e => {
+                    let votes = {}
+                    db.read(room).then(players => players.forEach(e => votes[e.sid] = e.votes.length))
+
+                    sockets[e.sid].emit("votes", votes)
+                })
+            }, 60000 * content.length + 60000)
         } else {
 
             socket.emit("error", 1)
         }
     })
 
-    socket.on("chance", () => {
+    /*socket.on("chance", () => {
         messages[room]++;
         let content;
         db.read(room).then(c=>content=c)
         content[messages[room] % content.length].emit("chance")
-    })
+    })*/
 
 
+    //Message and DM handling
     socket.on("msg", data => {
-        db.read(room).then(c=>c.forEach(e => { sockets[e.sid].emit("msg", data) }))
+        db.read(room).then(c => c.forEach(e => { sockets[e.sid].emit("msg", data) }))
     })
-
-
     socket.on("dm", d => {
         d.users.forEach(element => {
             sockets[element].emit("msg", d.msg)
         })
     })
 
+    //vote handling
+    socket.on("vote", id => {
+        db.vote(id, socket.id, room)//votes[room][id]++ 
+    })
 
-    socket.on("vote", id => { votes[room][id]++ })
-
-
+    //Disconnection handler
     socket.on("disconnect", () => {
 
         db.delete(socket.id, room);
-
-        db.read(room).then(c=>c.forEach(e => { sockets[e.sid].emit("members", members) }))
         delete sockets[socket.id]
+
+        let members = []
+        db.read(room).then(cont => cont.forEach(e => { members.push({ id: e.sid, name: e.name }) }))
+
+        db.read(room).then(c => c.forEach(e => { sockets[e.sid].emit("members", members) }))
         console.log(socket.id, " Left ", room)
-        db.read(room).then(cont=>console.log(cont))
+        db.read(room).then(cont => console.log(cont))
     })
 })
 
-
+//Common You know this shit
 http.listen(8080, "0.0.0.0")
